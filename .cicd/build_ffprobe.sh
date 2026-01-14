f7ca61ef67e045f928b122c3a04884b74ec5cf18
fleandro1 <fleandro1@yahoo.com>
2026-01-14 21:40:33 +0000

    Change ffprobe/ffmpeg version.

--- .cicd/build_ffprobe.sh
+++ .cicd/build_ffprobe.sh
@@ -2,9 +2,9 @@
 
 # Pin to a specific FFmpeg build version to avoid Lambda layer size limit issues.
 # Version: autobuild-2025-12-30-12-55 (131MB compressed)
-FFMPEG_VERSION="autobuild-2025-12-30-12-55"
-FFMPEG_FILENAME="ffmpeg-N-122292-gee2eb6ced8-linux64-gpl.tar.xz"
-FFMPEG_SHA256="743350f5b5fc489c727e7fbf0654d2c787841e743fef8d012b505e02ba4fd548"  # pragma: allowlist secret
+FFMPEG_VERSION="autobuild-2026-01-03-18-27"
+FFMPEG_FILENAME="ffmpeg-N-122344-g649a4e98f4-linux64-gpl.tar.xz"
+FFMPEG_SHA256="6aaa73f41175af562b990e10900789c9a0d3d9d8547d16a5aedc4825ec8ca23e"  # pragma: allowlist secret
 
 BASE_DIR=$(pwd)
 TEMP_DIR=$(mktemp -d)
